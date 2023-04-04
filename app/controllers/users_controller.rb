5064beec9d43aa380063eb1b7ee66926f37cf9c5
Alejandra Landa <aleriquene@gmail.com>
2023-04-04 19:22:00 +0000

    finished fixing all of the user errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
-
-  def index
+  def home
+    redirect_to("/users")
+  end
+  def all_users
     matching_users = User.all
     @users = matching_users.order(:created_at)
 
-    render({ :template => "users_templates/all_users.html.erb"})
+    render({ :template => "user_templates/all_users.html.erb"})
   end
   
   def show
@@ -16,21 +18,24 @@ class UsersController < ApplicationController
   end
   
   def create
+    insert_username = params.fetch("insert_username")
     user = User.new
-    user.username = params.fetch("query_username")
+    user.username = insert_username
     user.save
     
     redirect_to("/users/#{user.username}")
   end
   
   def update
-    user_id = params.fetch("user_id")
-    matching_users = User.where({ :id => user_id })
+    user_name = params.fetch("user_name")
+    matching_users = User.where({ :username => user_name })
     the_user = matching_users.at(0)
-    
-    the_user.username = params.fetch("query_username")
+
+    query_username = params.fetch("query_username")
+
+    the_user.username = query_username
     the_user.save
-    redirect_to("/users/#{user.username}")
+    redirect_to("/users/#{the_user.username}")
   end
 
 end
